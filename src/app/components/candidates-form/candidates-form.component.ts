a2bb8cfc83deb230a51cbebaed1b180afafb645e
AlexPerezDeveloper <alexperez.developer@gmail.com>
2024-09-01 02:26:50 +0200

    Added dummy content on table and submit functionalities

--- src/app/components/candidates-form/candidates-form.component.ts
+++ src/app/components/candidates-form/candidates-form.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-candidates-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './candidates-form.component.html',
   styleUrl: './candidates-form.component.scss'
 })
 export class CandidatesFormComponent {
+  candidatesForm: FormGroup;
 
+  constructor(){
+    this.candidatesForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      userExcel: new FormControl('', [Validators.required]),
+
+    })
+  }
+  onSubmit(){
+    if(this.candidatesForm.valid){
+      console.log(this.candidatesForm.value);
+    }else{
+      console.error("Error: Existe un problema con la validación de los campos del formulario.");
+    }
+  }
+  
 }
--- src/app/components/candidates-table/candidates-table.component.ts
+++ src/app/components/candidates-table/candidates-table.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+
+interface Candidate{
+  firstName: string,
+  lastName: string,
+  seniority: string,
+  years: number,
+  availability:boolean,
+}
 @Component({
   selector: 'app-candidates-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './candidates-table.component.html',
   styleUrl: './candidates-table.component.scss'
 })
-export class CandidatesTableComponent {
 
+
+
+export class CandidatesTableComponent {
+dummyCandidates: Candidate[] = [
+  {firstName: "Alex", lastName: "Pérez", seniority: "senior", years: 8, availability: true},
+  {firstName: "Pepe", lastName: "Marzo", seniority: "junior", years: 1, availability: false},
+  {firstName: "Maria", lastName: "Martinez", seniority: "senior", years: 4, availability: false},
+  {firstName: "Ana", lastName: "Ramirez", seniority: "junior", years: 3, availability: true},
+]
 }
